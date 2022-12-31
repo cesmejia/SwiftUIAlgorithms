4cbf75595e8ee68834a3ce063519b7fae76ce9ad
cesmejia <cesmejia8@gmail.com>
2022-12-30 23:30:29 -0600

    Initial Commit

---
+++ SwiftUIAlgorithms/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUIAlgorithms
+//
+//  Created by Cesar Mejia Valero on 12/30/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIAlgorithms/SwiftUIAlgorithmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAlgorithmsApp.swift
+//  SwiftUIAlgorithms
+//
+//  Created by Cesar Mejia Valero on 12/30/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAlgorithmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
